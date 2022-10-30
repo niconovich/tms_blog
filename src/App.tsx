1e19ad598c69b4cb66696724345debbee764fce8
niconovich <niconovich@gmail.com>
2022-10-30 22:15:27 +0300

    feat: add PostCard component homework-38

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React,{useState} from 'react';
 import './App.scss';
 
-import { MyButton } from  './Components/MyButton/MyButton';
-
-import {myAllButtons} from './Components/MyButton/MyButton.data';
-import {Icon} from './Components/icon/icon'
-import  {Input} from './Components/Input/Input'
-
+import {postData} from './data'
+import {PostСard} from "./Components/PostСard/PostСard";
 
 export const App=() => {
     const [error,setError]=useState(false)
@@ -17,13 +13,12 @@ export const App=() => {
     }
   return (
     <div className="App">
-      <MyButton params={myAllButtons.primary} />
-        <Icon name='up'/>
-      <form onClick={onSubmit}>
-          <Input label='tercn'  textError='error' placeholder='ddddddd' />
-          <button>Submit</button>
-      </form>
+        <div>
+            <PostСard typePostСard='default' id={postData.id} author={postData.author} title={postData.title} image={postData.image} text={postData.text} date={postData.date} lesson_num={postData.lesson_num} />
+            <PostСard typePostСard='average' id={postData.id} author={postData.author} title={postData.title} image={postData.image} text={postData.text} date={postData.date} lesson_num={postData.lesson_num}/>
+            <PostСard typePostСard='small' id={postData.id} author={postData.author} title={postData.title} image={postData.image} text={postData.text} date={postData.date} lesson_num={postData.lesson_num} />
 
+        </div>
     </div>
   );
 }
